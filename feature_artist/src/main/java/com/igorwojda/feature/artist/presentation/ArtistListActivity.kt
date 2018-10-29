a06115f8668e712d1a847c986c3217f60276f642
igor.wojda <igor.wojda@gmail.com>
2018-10-29 18:38:32 +0000

    Added feature artist

---
+++ feature_artist/src/main/java/com/igorwojda/feature/artist/presentation/ArtistListActivity.kt
@@ -0,0 +1,12 @@
+package com.igorwojda.feature.artist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtistListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
