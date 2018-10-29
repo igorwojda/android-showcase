41282f58dce21c312eadbd9a3798d7194807397c
igor.wojda <igor.wojda@gmail.com>
2018-10-29 18:44:14 +0000

    restored minify enabled^

--- feature_artist/build.gradle.kts
+++ feature_artist/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles("proguard-android.txt", "proguard-rules.pro")
 
         }
