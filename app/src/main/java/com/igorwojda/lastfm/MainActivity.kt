24c10ff7640c0b3187a90c9b9ad15ebddedfc1a7
igor.wojda <igor.wojda@gmail.com>
2018-11-02 02:54:37 +0000

    added listener in main activity

--- app/src/main/java/com/igorwojda/lastfm/MainActivity.kt
+++ app/src/main/java/com/igorwojda/lastfm/MainActivity.kt
@@ -2,7 +2,8 @@ package com.igorwojda.lastfm
 
 import android.content.Intent
 import android.os.Bundle
-import com.igorwojda.feature.artist.presentation.ArtistListActivity
+import com.igorwojda.lastfm.feature.album.presentation.AlbumListActivity
+import com.igorwojda.lastfm.feature.artist.presentation.ArtistListActivity
 import com.igorwojda.lastfm.feature.base.presentation.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +14,11 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        launchFeatureAlbumButton.setOnClickListener {
+            Intent(this, AlbumListActivity::class.java).also { intent ->
+                startActivity(intent)
+            }
+        }
         launchFeatureArtistButton.setOnClickListener {
             Intent(this, ArtistListActivity::class.java).also { intent ->
                 startActivity(intent)
