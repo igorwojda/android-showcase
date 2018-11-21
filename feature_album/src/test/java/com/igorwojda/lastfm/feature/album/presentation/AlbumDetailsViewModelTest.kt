37506030d4e7fa04c06bcfc7eef14594c244c29e
igor.wojda <igor.wojda@gmail.com>
2018-11-21 00:23:47 +0000

    added UseCase tests^

---
+++ feature_album/src/test/java/com/igorwojda/lastfm/feature/album/presentation/AlbumDetailsViewModelTest.kt
@@ -0,0 +1,41 @@
+package com.igorwojda.lastfm.feature.album.presentation
+
+import com.igorwojda.lastfm.feature.album.domain.usecase.GetAlbumUseCase
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class AlbumDetailsViewModelTest {
+    companion object {
+        const val ALBUM_ID = 1
+    }
+
+    @Mock
+    lateinit var mockGetAlbumUseCase: GetAlbumUseCase
+
+    private lateinit var cut: AlbumDetailsViewModel
+
+    @Before
+    fun setUp() {
+        cut = AlbumDetailsViewModel(
+            ALBUM_ID,
+            mockGetAlbumUseCase
+        )
+    }
+
+    @Test
+    fun `when execute then getProducts`() {
+        runBlocking {
+            // when
+            cut.init()
+
+            // then
+            verify(mockGetAlbumUseCase).execute(ALBUM_ID)
+        }
+    }
+}
---
+++ feature_album/src/test/java/com/igorwojda/lastfm/feature/album/presentation/AlbumListViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.igorwojda.lastfm.feature.album.presentation
+
+import com.igorwojda.lastfm.feature.album.domain.usecase.GetAlbumListUseCase
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class AlbumListViewModelTest {
+    @Mock
+    lateinit var mockGetAlbumListUseCase: GetAlbumListUseCase
+
+    private lateinit var cut: AlbumListViewModel
+
+    @Before
+    fun setUp() {
+        cut = AlbumListViewModel(mockGetAlbumListUseCase)
+    }
+
+    @Test
+    fun `when execute then getProducts`() {
+        runBlocking {
+            // when
+            cut.init()
+
+            // then
+            verify(mockGetAlbumListUseCase).execute()
+        }
+    }
+}
+
