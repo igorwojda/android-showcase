209f40892662e947554eb83c2b3943d96b3d8673
igor.wojda <igor.wojda@gmail.com>
2018-11-25 20:34:09 +0000

    Clean up

--- feature_base/src/main/java/com/igorwojda/lastfm/feature/base/presentation/BaseFragment.kt
+++ feature_base/src/main/java/com/igorwojda/lastfm/feature/base/presentation/BaseFragment.kt
@@ -1,4 +1,4 @@
-package com.igorwojda.minimercari.feature.base.presentation
+package com.igorwojda.lastfm.feature.base.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import timber.log.Timber
 
-abstract class BaseFragment : Fragment(), KodeinAware {
+abstract class BaseFragment : Fragment(), LifecycleOwner, KodeinAware {
     override val kodein by closestKodein()
 
     @get:LayoutRes
