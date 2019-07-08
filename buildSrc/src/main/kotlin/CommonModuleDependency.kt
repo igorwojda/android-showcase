0a8c369217f80496f6efb1a43af50f1212685236
igor.wojda <igor.wojda@gmail.com>
2019-07-08 21:35:10 +0100

    Updated readme

--- buildSrc/src/main/kotlin/CommonModuleDependency.kt
+++ buildSrc/src/main/kotlin/CommonModuleDependency.kt
@@ -2,8 +2,9 @@ import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
-// Define common dependencies, so they can be easily updated across feature modules
-
+/*
+Define common dependencies, so they can be easily updated across feature modules
+ */
 fun DependencyHandler.addCommonDependencies() {
     implementation(LibraryDependency.kotlin)
     implementation(LibraryDependency.kodein)
@@ -43,8 +44,8 @@ fun DependencyHandler.addTestDependencies() {
 }
 
 /*
- *
  * These extensions mimic the extensions that are generated on the fly by Gradle.
+ * They are used here to provide above dependency syntax that mimics Gradle Kotlin DSL syntax in module\build.gradle.kts files.
  */
 private fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? =
     add("implementation", dependencyNotation)
