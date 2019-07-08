1490aa0a71acc48fff88f56e00673acc35dc76c6
igor.wojda <igor.wojda@gmail.com>
2019-07-08 19:25:06 +0100

    Updated kodein module names

--- feature_album/src/main/java/com/igorwojda/lastfm/feature/album/AlbumModule.kt
+++ feature_album/src/main/java/com/igorwojda/lastfm/feature/album/AlbumModule.kt
@@ -1,12 +1,14 @@
 package com.igorwojda.lastfm.feature.album
 
-import com.igorwojda.lastfm.feature.album.data.albumDataModule
-import com.igorwojda.lastfm.feature.album.domain.albumDomainModule
-import com.igorwojda.lastfm.feature.album.presentation.albumPresentationModule
+import com.igorwojda.lastfm.feature.album.data.dataModule
+import com.igorwojda.lastfm.feature.album.domain.domainModule
+import com.igorwojda.lastfm.feature.album.presentation.presentationModule
 import org.kodein.di.Kodein
 
-val albumModule = Kodein.Module("albumModule") {
-    import(albumPresentationModule)
-    import(albumDomainModule)
-    import(albumDataModule)
+internal const val FEATURE_NAME = "featureAlbum"
+
+val albumModule = Kodein.Module("${FEATURE_NAME}Module") {
+    import(presentationModule)
+    import(domainModule)
+    import(dataModule)
 }
