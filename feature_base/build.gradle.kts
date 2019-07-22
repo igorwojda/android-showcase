0e555904d0765a61445af860ab8595d6ea2fe9ab
igor.wojda <igor.wojda@gmail.com>
2019-07-22 21:48:18 +0100

    Simplified gradle config

--- feature_base/build.gradle.kts
+++ feature_base/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     defaultConfig {
         minSdkVersion(AndroidConfig.minSdkVersion)
         targetSdkVersion(AndroidConfig.targetSdkVersion)
-        buildToolsVersion(AndroidConfig.buildToolsVersion)
 
         versionCode = AndroidConfig.versionCode
         versionName = AndroidConfig.versionName
