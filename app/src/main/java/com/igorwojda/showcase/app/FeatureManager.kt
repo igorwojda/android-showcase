9fae5ea3945039e016ec97a7365ea325b5b772c3
igor.wojda <igor.wojda@gmail.com>
2019-07-28 00:03:29 +0100

    updated desc

--- app/src/main/java/com/igorwojda/showcase/app/FeatureManager.kt
+++ app/src/main/java/com/igorwojda/showcase/app/FeatureManager.kt
@@ -2,6 +2,9 @@ package com.igorwojda.showcase.app
 
 import com.igorwojda.showcase.app.gateway.AlbumGateway
 
+// Dynamic Feature modules require reversed dependency (dynamic feature module depends on app module)
+// This means we have to use reflection to access module content
+// See: https://medium.com/mindorks/dynamic-feature-modules-the-future-4bee124c0f1
 @Suppress("detekt.UnsafeCast")
 object FeatureManager {
 
