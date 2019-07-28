73a82ee8463fea7fd3558dedd75cbfcef92ea1fa
igor.wojda <igor.wojda@gmail.com>
2019-07-28 09:33:04 +0100

    Updated resource name

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -2,7 +2,7 @@ object AndroidConfig {
     const val COMPILE_SDK_VERSION = 28
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 28
-    const val BUILD_TOOLS_VERSION = "28.0.3"
+    const val BUILD_TOOLS_VERSION = "29.0.0"
 
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
