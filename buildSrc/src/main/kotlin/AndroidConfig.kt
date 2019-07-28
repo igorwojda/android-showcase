977706b29a672cef6fef10a43484a6be9b576640
igor.wojda <igor.wojda@gmail.com>
2019-07-28 12:09:38 +0100

    Test build

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,7 +1,7 @@
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 28
+    const val COMPILE_SDK_VERSION = 29
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 28
+    const val TARGET_SDK_VERSION = 29
     const val BUILD_TOOLS_VERSION = "29.0.0"
 
     const val VERSION_CODE = 1
