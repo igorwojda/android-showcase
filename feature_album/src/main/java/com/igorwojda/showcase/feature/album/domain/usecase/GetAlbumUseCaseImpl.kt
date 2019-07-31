431f9869056f4538d595e9498ceb6780b13ed8f3
igor.wojda <igor.wojda@gmail.com>
2019-07-31 23:54:42 +0100

    Removed redundant interface for UseCase

--- feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/usecase/GetAlbumUseCaseImpl.kt
+++ feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/usecase/GetAlbumUseCaseImpl.kt
@@ -3,19 +3,10 @@ package com.igorwojda.showcase.feature.album.domain.usecase
 import com.igorwojda.showcase.feature.album.domain.model.AlbumDomainModel
 import com.igorwojda.showcase.feature.album.domain.repository.AlbumRepository
 
-internal interface GetAlbumUseCase {
-
-    suspend fun execute(
-        artistName: String,
-        albumName: String,
-        mbId: String?
-    ): AlbumDomainModel?
-}
-
-internal class GetAlbumUseCaseImpl(
+internal class GetAlbumUseCase(
     private val albumRepository: AlbumRepository
-) : GetAlbumUseCase {
-    override suspend fun execute(
+) {
+    suspend fun execute(
         artistName: String,
         albumName: String,
         mbId: String?
--- feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/usecase/SearchAlbumUseCaseImpl.kt
+++ feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/usecase/SearchAlbumUseCaseImpl.kt
@@ -3,15 +3,11 @@ package com.igorwojda.showcase.feature.album.domain.usecase
 import com.igorwojda.showcase.feature.album.domain.model.AlbumDomainModel
 import com.igorwojda.showcase.feature.album.domain.repository.AlbumRepository
 
-internal interface SearchAlbumUseCase {
-    suspend fun execute(phrase: String): List<AlbumDomainModel>
-}
-
-internal class SearchAlbumUseCaseImpl(
+internal class SearchAlbumUseCase(
     private val albumRepository: AlbumRepository
-) : SearchAlbumUseCase {
+) {
 
-    override suspend fun execute(phrase: String): List<AlbumDomainModel> =
+    suspend fun execute(phrase: String): List<AlbumDomainModel> =
         if (phrase.isBlank()) {
             listOf()
         } else {
