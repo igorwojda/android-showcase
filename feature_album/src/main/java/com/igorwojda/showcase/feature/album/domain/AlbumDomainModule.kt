8f7afb0cedad9756b1267dd03402ee9368c1733f
igor.wojda <igor.wojda@gmail.com>
2019-08-04 12:53:39 +0100

    Simplified DI^

--- feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/AlbumDomainModule.kt
+++ feature_album/src/main/java/com/igorwojda/showcase/feature/album/domain/AlbumDomainModule.kt
@@ -6,11 +6,11 @@ import com.igorwojda.showcase.feature.album.domain.usecase.SearchAlbumUseCase
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
-import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
 internal val domainModule = Kodein.Module("${FEATURE_NAME}DomainModule") {
 
-    bind<SearchAlbumUseCase>() with provider { SearchAlbumUseCase(instance()) }
+    bind() from singleton { SearchAlbumUseCase(instance()) }
 
-    bind<GetAlbumUseCase>() with provider { GetAlbumUseCase(instance()) }
+    bind() from singleton { GetAlbumUseCase(instance()) }
 }
