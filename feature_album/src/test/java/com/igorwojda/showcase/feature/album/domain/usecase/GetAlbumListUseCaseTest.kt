59494ee49b0bb2f9acaa5fce14651862e2335a60
igor.wojda <igor.wojda@gmail.com>
2019-08-05 19:29:20 +0100

    Test to filter albums without images

--- feature_album/src/test/java/com/igorwojda/showcase/feature/album/domain/usecase/GetAlbumListUseCaseTest.kt
+++ feature_album/src/test/java/com/igorwojda/showcase/feature/album/domain/usecase/GetAlbumListUseCaseTest.kt
@@ -1,10 +1,13 @@
 package com.igorwojda.showcase.feature.album.domain.usecase
 
 import com.igorwojda.showcase.feature.album.data.repository.AlbumRepositoryImpl
+import com.igorwojda.showcase.feature.album.domain.enum.AlbumDomainImageSize
+import com.igorwojda.showcase.feature.album.domain.model.AlbumDomainModel
+import com.igorwojda.showcase.feature.album.domain.model.AlbumImageDomainModel
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.given
-import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.runBlocking
+import org.amshove.kluent.shouldEqual
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,13 +31,41 @@ class GetAlbumListUseCaseTest {
     fun `return list of albums`() {
         runBlocking {
             // given
-            given(mockAlbumRepository.searchAlbum(any())).willReturn(listOf())
+            val albums = getAlbumList()
+            given(mockAlbumRepository.searchAlbum(any())).willReturn(albums)
 
             // when
-            cut.execute()
+            val result = cut.execute()
 
             // then
-            verify(mockAlbumRepository).searchAlbum(any())
+            result shouldEqual albums
         }
     }
+
+    @Test
+    fun `filter albums without images`() {
+        runBlocking {
+            // given
+            val albumWithImage = getAlbum(listOf(AlbumDomainImageSize.EXTRA_LARGE))
+            val albumWithoutImage = getAlbum(null)
+            val albums = listOf(albumWithImage, albumWithoutImage)
+            given(mockAlbumRepository.searchAlbum(any())).willReturn(albums)
+
+            // when
+            val result = cut.execute()
+
+            // then
+            result shouldEqual listOf(albumWithImage)
+        }
+    }
+
+    private fun getAlbumList() = listOf(getAlbum(listOf(AlbumDomainImageSize.EXTRA_LARGE)))
+
+    private fun getAlbum(imageSizes: List<AlbumDomainImageSize>?): AlbumDomainModel {
+        val images = imageSizes?.map { getAlbumImage(it) } ?: listOf()
+        return AlbumDomainModel("name", "artist", images, null, null)
+    }
+
+    private fun getAlbumImage(size: AlbumDomainImageSize = AlbumDomainImageSize.EXTRA_LARGE) =
+        AlbumImageDomainModel("url", size)
 }
