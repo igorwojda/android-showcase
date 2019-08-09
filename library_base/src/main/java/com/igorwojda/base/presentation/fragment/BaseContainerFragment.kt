a88a1d824247e7f5e3933b9704cc21a99a84b4ae
igor.wojda <igor.wojda@gmail.com>
2019-08-09 18:44:24 +0100

    Retrieve ViewModel by reflection 2

--- library_base/src/main/java/com/igorwojda/base/presentation/fragment/BaseContainerFragment.kt
+++ library_base/src/main/java/com/igorwojda/base/presentation/fragment/BaseContainerFragment.kt
@@ -11,7 +11,9 @@ import androidx.navigation.NavArgs
 import androidx.navigation.NavArgsLazy
 import com.igorwojda.base.presentation.viewmodel.BaseViewModel
 import timber.log.Timber
+import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
 
 abstract class BaseContainerFragment : InjectionFragment() {
@@ -21,49 +23,62 @@ abstract class BaseContainerFragment : InjectionFragment() {
 
     protected abstract val viewModel: BaseViewModel
 
+    private val navArgsReflection by navArgsReflection()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(layoutResourceId, null).also {
             Timber.v("onCreateView ${javaClass.simpleName}")
         }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
-            viewModel.setAgrs(getNavArgsForFragment())
+//            viewModel.setAgrs(getNavArgsForFragment())
+            viewModel.setAgrs(navArgsReflection)
             viewModel.loadData()
         }
     }
 }
 
 /**
- * This method will be called during initialization to provide
- * arguments to construct an [NavArgs] instance via reflection.
- * If no arguments where passed to fragment this method will return null.
+ * This method will be called during initialization to provide [NavArgs]
+ * instance via reflection. If no arguments where passed to fragment this
+ * method will return null.
  */
-@MainThread
-fun Fragment.getNavArgsForFragment(): NavArgs? {
-    // SafeArgs plugin adds "Agrs" suffix to the fragment class eg.
-    // If com.abc.MyFragment class has arguments defined in nav_graph.xml then
-    // class com.abc.MyFragmentArgs will be generated.
 
-    val localArguments = arguments ?: return null
+// This code servers as a wrapper for NavArgsLazy class.
 
-    val canonicalName = "${this::class.java.canonicalName}Args"
-    @Suppress("UNCHECKED_CAST")
-    val navArgsClass = requireNotNull(getClass(canonicalName) as? KClass<NavArgs>) {
-        "Arguments where passed to a fragment, but corresponding argument class $canonicalName does not exist. Arguments: $localArguments"
+
+class NavArgsReflection<Args : NavArgs?>(
+    private val navArgsClass: KClass<out Fragment>,
+    private val arguments: () -> Bundle?
+) :
+    ReadOnlyProperty<Any?, Args?> {
+
+    private var navArgsLazy: Args? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Args? {
+
+        if (navArgsLazy == null) {
+            // SafeArgs plugin adds "Agrs" suffix to the fragment class eg.
+            // If com.abc.MyFragment class has arguments defined in nav_graph.xml then
+            // class com.abc.MyFragmentArgs will be generated.
+
+            arguments.invoke() ?: return null
+
+            val canonicalName = "${navArgsClass.java.canonicalName}Args"
+            @Suppress("UNCHECKED_CAST")
+            val navArgsClass = requireNotNull(getClass(canonicalName) as? KClass<NavArgs>) {
+                "Arguments where passed to a fragment, but corresponding argument class $canonicalName does not exist. Arguments: $arguments"
+            }
+
+            // Let's check if Args class actually exists
+            navArgsLazy = NavArgsLazy(navArgsClass) { arguments.invoke() ?: throw  Exception("AAA") }.value as Args
+        }
+
+        return navArgsLazy
     }
-
-    // Let's check if Args class actually exists
-    val navArgs by NavArgsLazy(navArgsClass) { localArguments }
-    return navArgs
 }
 
 fun getClass(className: String): KClass<*>? = try {
@@ -72,8 +87,5 @@ fun getClass(className: String): KClass<*>? = try {
     null
 }
 
-
 @MainThread
-inline fun <reified Args : NavArgs> Fragment.navArgsByReflection() = NavArgsLazy(Args::class) {
-    arguments ?: throw IllegalStateException("Fragment $this has null arguments")
-}
+fun Fragment.navArgsReflection() = NavArgsReflection<NavArgs>(this::class) { arguments }
