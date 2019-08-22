1a8b0235250b50219e5a348b54b50918de221a7f
igor.wojda <igor.wojda@gmail.com>
2019-08-22 23:53:27 +0100

    simplified desc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,11 +2,7 @@ plugins {
     `kotlin-dsl`
 }
 
-/*
- * Fix for "Gradle 4.10.2 can't resolve dependency kotlin-scripting-compiler-embeddable"
- * (With Kotlin 1.2.60, the Kotlin Gradle Plugin driving the kotlin compiler. Now it requires extra dependencies that
- * aren't required by Gradle Kotlin DSL scripts alone and aren't embedded into Gradle)
- */
+// The kotlin-dsl plugin requires a repository to be declared
 repositories {
     jcenter()
 }
