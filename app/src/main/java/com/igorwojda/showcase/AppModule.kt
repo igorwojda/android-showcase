6cf03336090701627fc1d860981a56821abec6c2
igor.wojda <igor.wojda@gmail.com>
2019-08-27 20:37:33 +0100

    resValue -> buildConfigField

--- app/src/main/java/com/igorwojda/showcase/AppModule.kt
+++ app/src/main/java/com/igorwojda/showcase/AppModule.kt
@@ -1,33 +1,20 @@
 package com.igorwojda.showcase
 
-import android.content.Context
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.igorwojda.showcase.app.data.retrofit.AuthenticationInterceptor
 import com.igorwojda.showcase.app.data.retrofit.UserAgentInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.kodein.di.Kodein
-import org.kodein.di.bindings.Provider
-import org.kodein.di.erased
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.singleton
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-private const val TAG_API_BASE_URL = "TAG_API_BASE_URL"
-private const val TAG_API_TOKEN = "TAG_API_TOKEN"
-
 val appModule = Kodein.Module("appModule") {
 
-    bind(TAG_API_BASE_URL) from Provider(
-        erased<Context>(),
-        erased()
-    ) { context.getString(R.string.build_param_api_base_url) }
-
-    bind(TAG_API_TOKEN) from Provider(erased<Context>(), erased()) { context.getString(R.string.build_param_api_token) }
-
-    bind() from singleton { AuthenticationInterceptor(instance(TAG_API_TOKEN)) }
+    bind() from singleton { AuthenticationInterceptor(BuildConfig.GRADLE_API_TOKEN) }
 
     bind() from singleton { UserAgentInterceptor() }
 
@@ -52,7 +39,7 @@ val appModule = Kodein.Module("appModule") {
 
     bind<Retrofit>() with singleton {
         instance<Retrofit.Builder>()
-            .baseUrl(instance<String>(TAG_API_BASE_URL))
+            .baseUrl(BuildConfig.GRADLE_API_BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
             .client(instance())
             .build()
