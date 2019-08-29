44fad04bfc01e729473bdd3cc39b42148680e290
igor.wojda <igor.wojda@gmail.com>
2019-08-29 19:55:15 +0100

    Retrieve module names from ModuleDependency object

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -1,24 +1,8 @@
 import kotlin.reflect.full.memberProperties
 
-const val FEATURE_PREFIX = ":feature_"
+private const val FEATURE_PREFIX = ":feature_"
 
 // "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject".
-
-// enum class ModuleDependency(val moduleName: String) {
-//     APP(":app"),
-//     FEATURE_ALBUM(":feature_album"),
-//     FEATURE_PROFILE(":feature_profile"),
-//     FEATURE_FAVOURITE(":feature_favourite"),
-//     LIBRARY_BASE(":library_base"),
-//     LIBRARY_TEST_UTILS(":library_test_utils");
-//
-//     // There is no easy way to retrieve dynamic feature modules from gradle based on plugin type ("com.android.dynamic-feature",
-//     // so we have to retrieve them from enum
-//     fun getDynamicFeatures() = enumValues<ModuleDependency>().filter { it.moduleName.startsWith(FEATURE_PREFIX) }
-//
-//     fun getAll() = enumValues<ModuleDependency>()
-// }
-
 object ModuleDependency {
     const val APP = ":app"
     const val FEATURE_ALBUM = ":feature_album"
@@ -27,18 +11,14 @@ object ModuleDependency {
     const val LIBRARY_BASE = ":library_base"
     const val LIBRARY_TEST_UTILS = ":library_test_utils"
 
-    val a = ModuleDependency::class.memberProperties.forEach { println(it) }
-    // map { it.getter.call(ModuleDependency::class.objectInstance).toString() }
+    // There is no easy way to retrieve dynamic feature modules from gradle based on plugin type ("com.android.dynamic-feature")
+    // but we can retrieve them from above consts via reflection
+    fun getAllModules() = ModuleDependency::class.memberProperties
+        .filter { it.isConst }
+        .map { it.getter.call().toString() }
+        .toSet()
 
-    val dynamicFeatureModules = setOf(
-        ModuleDependency.FEATURE_ALBUM,
-        ModuleDependency.FEATURE_FAVOURITE,
-        ModuleDependency.FEATURE_PROFILE
-    )
-
-    val modules = setOf(
-        ModuleDependency.FEATURE_ALBUM,
-        ModuleDependency.FEATURE_FAVOURITE,
-        ModuleDependency.FEATURE_PROFILE
-    )
+    fun getDynamicFeatureModules() = getAllModules()
+        .filter { it.startsWith(FEATURE_PREFIX) }
+        .toSet()
 }
