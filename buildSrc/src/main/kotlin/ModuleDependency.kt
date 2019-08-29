7110b9ccf54a7349cc2934e5f1b972088a0b10dd
igor.wojda <igor.wojda@gmail.com>
2019-08-29 20:11:01 +0100

    Updated comment formatting

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -2,7 +2,7 @@ import kotlin.reflect.full.memberProperties
 
 private const val FEATURE_PREFIX = ":feature_"
 
-// "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject".
+// "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject"
 object ModuleDependency {
     const val APP = ":app"
     const val FEATURE_ALBUM = ":feature_album"
@@ -11,8 +11,8 @@ object ModuleDependency {
     const val LIBRARY_BASE = ":library_base"
     const val LIBRARY_TEST_UTILS = ":library_test_utils"
 
-    // There is no easy way to retrieve dynamic feature modules from gradle based on plugin type ("com.android.dynamic-feature")
-    // but we can retrieve them from above consts via reflection
+    // There is no easy way to retrieve dynamic feature modules from gradle based on plugin type
+    // ("com.android.dynamic-feature") but we can retrieve them from above consts via reflection
     fun getAllModules() = ModuleDependency::class.memberProperties
         .filter { it.isConst }
         .map { it.getter.call().toString() }
