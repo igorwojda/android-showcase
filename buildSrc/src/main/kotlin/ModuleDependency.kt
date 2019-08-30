29ac9262958a63939b6ec7a101dace7c68969104
Igor Wojda <igor.wojda@gmail.com>
2019-08-30 08:31:03 +0100

    Updated comment (#46)

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -3,7 +3,9 @@ import kotlin.reflect.full.memberProperties
 private const val FEATURE_PREFIX = ":feature_"
 
 // "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject"
+@Suppress("unused")
 object ModuleDependency {
+    // All consts are accessed via reflection
     const val APP = ":app"
     const val FEATURE_ALBUM = ":feature_album"
     const val FEATURE_PROFILE = ":feature_profile"
