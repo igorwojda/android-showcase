92ef22b4a822bdbbe54b4b22d953f2f697937dd5
igor.wojda <igor.wojda@gmail.com>
2019-08-30 08:16:33 +0100

    Updated comment

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -3,7 +3,9 @@ import kotlin.reflect.full.memberProperties
 private const val FEATURE_PREFIX = ":feature_"
 
 // "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject"
+@Suppress("unused")
 object ModuleDependency {
+    // All consts are accessed via reflection
     const val APP = ":app"
     const val FEATURE_ALBUM = ":feature_album"
     const val FEATURE_PROFILE = ":feature_profile"
@@ -11,7 +13,7 @@ object ModuleDependency {
     const val LIBRARY_BASE = ":library_base"
     const val LIBRARY_TEST_UTILS = ":library_test_utils"
 
-    // Unused function false positive
+    // False positive" function can be private"
     // See: https://youtrack.jetbrains.com/issue/KT-33610
     fun getAllModules() = ModuleDependency::class.memberProperties
         .filter { it.isConst }
