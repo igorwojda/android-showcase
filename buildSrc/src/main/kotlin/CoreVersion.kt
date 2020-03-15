9bd6984c50e2da9acc7e54c907ed206cdfe3e015
Igor Wojda <igor.wojda@gmail.com>
2020-03-15 13:58:47 +0000

    Update Kotlin to 1.3.70 (#75)

--- buildSrc/src/main/kotlin/CoreVersion.kt
+++ buildSrc/src/main/kotlin/CoreVersion.kt
@@ -1,6 +1,6 @@
 // Versions consts that are used across libraries and Gradle plugins
 object CoreVersion {
-    const val KOTLIN = "1.3.61"
+    const val KOTLIN = "1.3.70"
     const val COROUTINES_ANDROID = "1.3.2"
     const val KTLINT = "0.36.0"
     const val NAVIGATION = "2.1.0"
