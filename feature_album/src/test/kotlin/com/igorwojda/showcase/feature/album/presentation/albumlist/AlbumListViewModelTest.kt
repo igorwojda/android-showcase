c25c64139c9d036d74ea45f57829b9bacdb6b83f
Igor <igor.wojda@gmail.com>
2021-02-08 14:46:28 +0100

    Updated formatting

--- feature_album/src/test/kotlin/com/igorwojda/showcase/feature/album/presentation/albumlist/AlbumListViewModelTest.kt
+++ feature_album/src/test/kotlin/com/igorwojda/showcase/feature/album/presentation/albumlist/AlbumListViewModelTest.kt
@@ -1,10 +1,10 @@
 package com.igorwojda.showcase.feature.album.presentation.albumlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.igorwojda.showcase.base.presentation.navigation.NavManager
 import com.igorwojda.showcase.feature.album.domain.model.AlbumDomainModel
 import com.igorwojda.showcase.feature.album.domain.usecase.GetAlbumListUseCase
 import com.igorwojda.showcase.feature.album.presentation.albumlist.AlbumListViewModel.ViewState
-import com.igorwojda.showcase.base.presentation.navigation.NavManager
 import com.igorwojda.showcase.library.testutils.CoroutineRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
