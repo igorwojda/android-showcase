5c5258af42c502c541c4ec38daac17f3a55d3a19
Igor <igor.wojda@gmail.com>
2021-02-12 10:36:43 +0100

    Fix comments

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.buildFileName = "build.gradle.kts"
 
-//// Set single lock file (gradle.lockfile)
-//// This preview feature should be enabled by default in Gradle 7
-//// More: https://docs.gradle.org/current/userguide/dependency_locking.html#single_lock_file_per_project
+// Set single lock file (gradle.lockfile)
+// This preview feature should be enabled by default in Gradle 7
+// More: https://docs.gradle.org/current/userguide/dependency_locking.html#single_lock_file_per_project
 enableFeaturePreview("ONE_LOCKFILE_PER_PROJECT")
 
 include(
