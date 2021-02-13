f0afe9a4c116b7c4f8a85830abb795986774ad90
Igor <igor.wojda@gmail.com>
2021-02-13 17:17:45 +0100

    Update detekt target version of the generated JVM bytecode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 
 import com.android.build.gradle.BaseExtension
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -81,6 +82,11 @@ fun Project.configureAndroid() {
 // JVM target applied to all Kotlin tasks across all sub-projects
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+}
+
+// Target version of the generated JVM bytecode. It is used for type resolution.
+tasks.withType<Detekt> {
+    this.jvmTarget = "1.8"
 }
 
 /*
