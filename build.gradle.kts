71aadaa3514001a7cf95495027450b8de067fc14
Igor <igor.wojda@gmail.com>
2021-02-17 19:05:44 +0100

    Remove static check task

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,30 +128,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Detekt> {
     this.jvmTarget = "1.8"
 }
-
-/*
-Mimics all static checks that run on CI.
-Note that this task is intended to run locally (not on CI), because on CI we prefer to have parallel execution
-and separate reports for each of the checks (multiple statuses eg. on github PR page).
- */
-task("staticCheck") {
-    group = "verification"
-    afterEvaluate {
-        // Filter modules with "lintDebug" task (non-Android modules do not have lintDebug task)
-        val lintTasks = subprojects.mapNotNull { "${it.name}:lintDebug" }
-
-        // Get modules with "testDebugUnitTest" task (app module does not have it)
-        val testTasks = subprojects.mapNotNull { "${it.name}:testDebugUnitTest" }
-            .filter { it != "app:testDebugUnitTest" }
-
-        // All task dependencies
-        val taskDependencies =
-            mutableListOf("app:assembleAndroidTest", "ktlintCheck", "detekt").also {
-                it.addAll(lintTasks)
-                it.addAll(testTasks)
-            }
-
-        // By defining Gradle dependency all dependent tasks will run before this "empty" task
-        dependsOn(taskDependencies)
-    }
-}
