9fdbf20adeeae47ed3cb8dfd593953b6cb44638d
Igor Wojda <igor.wojda@gmail.com>
2021-02-18 10:32:43 +0100

    Fix unknown destination crash (#153)

--- app/src/main/kotlin/com/igorwojda/showcase/app/presentation/NavHostActivity.kt
+++ app/src/main/kotlin/com/igorwojda/showcase/app/presentation/NavHostActivity.kt
@@ -35,7 +35,28 @@ class NavHostActivity : BaseActivity() {
 
     private fun initNavManager() {
         navManager.setOnNavEvent {
-            navController.navigate(it)
+
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)
+            val currentFragment = navHostFragment?.childFragmentManager?.fragments?.get(0)
+
+            /*
+            When trying to navigate to a destination that is not included in the current navigation graph
+            the app will crash:
+            java.lang.IllegalArgumentException: Navigation action/destination X cannot be found
+
+            This happens when a (second) navigation request is triggered from a fragment that is no longer the current
+            location (currentDestination) in the navController. In other words, when navigating from A to B, there is
+            a moment when:
+                - Fragment A is still active and showing.
+                - The navigation library already changed its current location,currentDestination, to Fragment B.
+
+            Crash happens when a second request to navigate from fragment A comes in at exactly this moment (usually due
+            to extremely fast clicking or multitouch), and it uses a destination that is not included in B’s graph.
+            More: https://medium.com/@ffvanderlaan/fixing-the-dreaded-is-unknown-to-this-navcontroller-68c4003824ce
+             */
+            if (navController.currentDestination?.id == currentFragment?.id) {
+                navController.navigate(it)
+            }
         }
     }
 }
