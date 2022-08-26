56a16c1b73b1e1835c2166de911a1df8994afe8e
Igor Wojda <igor.wojda@gmail.com>
2022-08-26 17:31:12 +0200

    upd desc

--- feature_base/src/main/java/com/igorwojda/showcase/base/extension/FragmentExt.kt
+++ feature_base/src/main/java/com/igorwojda/showcase/base/extension/FragmentExt.kt
@@ -12,20 +12,17 @@ import timber.log.Timber
  * the app will crash:
  * java.lang.IllegalArgumentException: Navigation action/destination X cannot be found
  *
- * This happens when a (second) navigation request is triggered from a fragment that is no longer the
- * current
+ * This happens when a (second) navigation request is triggered from a fragment that is no longer the current
  * location (currentDestination) in the navController. In other words, when navigating from A to B, there is
  * a moment when:
  *     - Fragment A is still active and showing.
  *     - The navigation library already changed its current location,currentDestination, to Fragment B.
  *
  * Crash happens when a second request to navigate from fragment A comes in at exactly this moment (usually
- *  due
- * to extremely fast clicking or multitouch), and it uses a destination that is not included in B’s graph.
+ *  due to extremely fast clicking or multitouch), and it uses a destination that is not included in B’s graph.
  * More: https://medium.com/@ffvanderlaan/fixing-the-dreaded-is-unknown-to-this-navcontroller-68c4003824ce
  *
- * This method navigates only if this is safely possible; when this Fragment is still the current
- * destination.
+ * This method navigates only if this is safely possible; when this Fragment is still the current destination.
  */
 fun Fragment.navigateSafe(directions: NavDirections, navOptions: NavOptions? = null) {
     if (canNavigate()) findNavController().navigate(directions, navOptions)
