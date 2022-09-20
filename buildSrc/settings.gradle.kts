0eeef62d101c047f86e89e89870ed8081d956f37
Igor Wojda <igor.wojda@gmail.com>
2022-09-20 17:31:05 +0200

    Remove buildSrc

--- buildSrc/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
-rootProject.name = ("buildSrc")
