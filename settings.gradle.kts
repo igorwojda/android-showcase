d386679f43fbd652288b6aab005c9fe65d67ddcd
Igor Wojda <igor.wojda@gmail.com>
2022-09-20 17:31:16 +0200

    Rearrange items

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
 rootProject.name = "android-showcase"
 
+include(
+    ":app",
+    ":feature_album",
+    ":feature_profile",
+    ":feature_favourite",
+    ":feature_base",
+    ":library_test_utils"
+)
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -18,13 +27,4 @@ dependencyResolutionManagement {
 // Generate type safe accessors when referring to other projects eg.
 // Before: implementation(project(":feature_album"))
 // After: implementation(projects.featureAlbum)
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-include(
-    ":app",
-    ":feature_album",
-    ":feature_profile",
-    ":feature_favourite",
-    ":feature_base",
-    ":library_test_utils"
-)
+// enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
