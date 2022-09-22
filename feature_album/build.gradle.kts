9e0678d72e0df8b5a4d8e7edb69a08e06ad4d9b2
Igor Wojda <igor.wojda@gmail.com>
2022-09-22 10:54:34 +0200

    Update target SDK

--- feature_album/build.gradle.kts
+++ feature_album/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
