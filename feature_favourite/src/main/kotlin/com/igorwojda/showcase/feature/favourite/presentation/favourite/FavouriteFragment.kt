726a27b95907031a88f1fbf28d1edf72fad47a18
Igor Wojda <igor.wojda@gmail.com>
2022-09-26 21:30:21 +0200

    Add Composable - Profile and Favourites Screen

--- feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/favourite/FavouriteFragment.kt
+++ feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/favourite/FavouriteFragment.kt
@@ -1,16 +1,69 @@
 package com.igorwojda.showcase.feature.favourite.presentation.favourite
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import by.kirich1409.viewbindingdelegate.viewBinding
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.igorwojda.showcase.base.presentation.compose.ShowcaseTheme
 import com.igorwojda.showcase.feature.favourite.R
-import com.igorwojda.showcase.feature.favourite.databinding.FragmentFavouritesBinding
 
-class FavouriteFragment : Fragment(R.layout.fragment_favourites) {
+class FavouriteFragment : Fragment() {
 
-    private val binding: FragmentFavouritesBinding by viewBinding()
-
-    override fun onResume() {
-        super.onResume()
-        binding.underConstructionAnimation.playAnimation()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                ShowcaseTheme {
+                    FavouriteScreen()
+                }
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+internal fun FavouriteScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .wrapContentHeight()
+            .wrapContentWidth()
+    ) {
+        Surface(color = Color.Blue) {
+            Text(
+                text = stringResource(R.string.under_construction),
+                fontSize = 18.sp
+            )
+        }
+        LottieAssetLoader("lottie_building_screen.json")
+    }
+}
+
+@Composable
+fun LottieAssetLoader(assetName: String) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.Asset(assetName))
+    LottieAnimation(
+        composition,
+        modifier = Modifier.requiredSize(150.dp)
+    )
 }
