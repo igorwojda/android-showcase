eb062bcea19cfe5e5971247926027c64b82002db
Igor Wojda <igor.wojda@gmail.com>
2022-10-14 06:28:52 +0200

    Clean up

--- feature_base/src/main/kotlin/com/igorwojda/showcase/base/presentation/compose/composable/View.kt
+++ feature_base/src/main/kotlin/com/igorwojda/showcase/base/presentation/compose/composable/View.kt
@@ -3,7 +3,6 @@ package com.igorwojda.showcase.base.presentation.compose.composable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,8 +25,7 @@ fun ProgressIndicator() {
         CircularProgressIndicator(
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(Dimen.spaceXXL),
-            color = MaterialTheme.colorScheme.primary
+                .size(Dimen.spaceXXL)
         )
     }
 }
