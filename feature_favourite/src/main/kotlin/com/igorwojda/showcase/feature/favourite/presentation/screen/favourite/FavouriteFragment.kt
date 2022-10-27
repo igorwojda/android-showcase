16fa433de07e5b50045c2eeda3c37ecaa971d8fc
Igor Wojda <igor.wojda@gmail.com>
2022-10-27 16:47:55 +0200

    Fix formatting

--- feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/screen/favourite/FavouriteFragment.kt
+++ feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/screen/favourite/FavouriteFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.igorwojda.showcase.base.presentation.activity.BaseFragment
 import com.igorwojda.showcase.base.presentation.compose.composable.UnderConstructionAnim
@@ -13,7 +14,7 @@ import com.igorwojda.showcase.base.presentation.compose.composable.UnderConstruc
 class FavouriteFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return ComposeView(requireContext()).apply {
+        return ComposeView(LocalContext.current).apply {
             setContent {
                 FavouriteScreen()
             }
