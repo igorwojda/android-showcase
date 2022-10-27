a2d75d6efc705361a31178dbb61b0b380eb78c1e
Igor Wojda <igor.wojda@gmail.com>
2022-10-27 21:36:14 +0200

    Update context

--- feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/screen/favourite/FavouriteFragment.kt
+++ feature_favourite/src/main/kotlin/com/igorwojda/showcase/feature/favourite/presentation/screen/favourite/FavouriteFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.igorwojda.showcase.base.presentation.activity.BaseFragment
 import com.igorwojda.showcase.base.presentation.compose.composable.UnderConstructionAnim
@@ -14,7 +13,7 @@ import com.igorwojda.showcase.base.presentation.compose.composable.UnderConstruc
 class FavouriteFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return ComposeView(LocalContext.current).apply {
+        return ComposeView(requireContext()).apply {
             setContent {
                 FavouriteScreen()
             }
