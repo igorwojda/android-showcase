55f7f4eeb16e1dc0e1b510fba710f92322f3a6be
Igor Wojda <igor.wojda@gmail.com>
2023-02-20 17:38:45 +0100

    Upd Toolchain

--- library_test_utils/build.gradle.kts
+++ library_test_utils/build.gradle.kts
@@ -30,6 +30,12 @@ android {
     }
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 dependencies {
     // implementation configuration is used here (instead of testImplementation) because this module is added as
     // testImplementation dependency inside other modules. Using implementation allows to write tests for test utilities.
