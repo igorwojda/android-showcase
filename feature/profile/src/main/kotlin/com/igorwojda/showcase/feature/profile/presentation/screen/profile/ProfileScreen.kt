df78cef1c3fb22863d6da6e408fa7d201f09b7a2
Igor Wojda <igor.wojda@gmail.com>
2025-09-08 11:45:51 +0200

    refactor screens to center under construction animation for consistency

--- feature/profile/src/main/kotlin/com/igorwojda/showcase/feature/profile/presentation/screen/profile/ProfileScreen.kt
+++ feature/profile/src/main/kotlin/com/igorwojda/showcase/feature/profile/presentation/screen/profile/ProfileScreen.kt
@@ -1,12 +1,21 @@
 package com.igorwojda.showcase.feature.profile.presentation.screen.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.igorwojda.showcase.feature.base.presentation.compose.composable.UnderConstructionAnim
 
 @Composable
 fun ProfileScreen() {
-    UnderConstructionAnim()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        UnderConstructionAnim()
+    }
 }
 
 @Preview
