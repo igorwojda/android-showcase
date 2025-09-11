c6f902a4037f50e172c9bf7e9328b54b95adfb21
Igor Wojda <igor.wojda@gmail.com>
2025-09-11 10:14:53 +0200

    Upd buildScr project name

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -2,7 +2,7 @@
 Fix for "Project accessors enabled, but root project name not explicitly set for 'buildSrc'"
 https://github.com/gradle/gradle/issues/30299
 */
-rootProject.name = "android-showcase"
+rootProject.name = "buildSrc"
 
 pluginManagement {
 
