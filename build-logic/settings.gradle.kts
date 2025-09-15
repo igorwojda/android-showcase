e56b9c98785bec750e1aaa253476f43b83a83f0f
Igor Wojda <igor.wojda@gmail.com>
2025-09-15 11:06:40 +0200

    Clean up

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -3,11 +3,13 @@ rootProject.name = "build-logic"
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
