353e55117764327058269a9c6f270f6e9cbb895a
Igor Wojda <igor.wojda@gmail.com>
2025-09-15 12:48:56 +0200

    Move deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 plugins {
     id("com.igorwojda.showcase.buildlogic.detekt")
     id("com.igorwojda.showcase.buildlogic.spotless")
+    
+    // Core Android and Kotlin plugins using version catalog
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.symbol.processing) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.test.logger) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.junit5.android) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,25 +12,12 @@ include(
 
 pluginManagement {
     includeBuild("build-logic")
-
+    
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
-}
-
-plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("com.adarshr.test-logger") version "4.0.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
-    id("com.diffplug.spotless") version "7.2.1" apply false
-    id("de.mannodermaus.android-junit5") version "1.13.4.0" apply false
 }
 
 @Suppress("UnstableApiUsage")
