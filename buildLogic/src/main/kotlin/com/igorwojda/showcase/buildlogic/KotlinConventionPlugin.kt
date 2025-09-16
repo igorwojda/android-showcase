e535004f340a2020982beec81c34bf65c156219b
Igor Wojda <igor.wojda@gmail.com>
2025-09-16 19:04:28 +0200

    Fix Build Warning (#682)
    * Fix build warning
    
    * Apply automatic code style fixes
    
    * Upd comment

--- buildLogic/src/main/kotlin/com/igorwojda/showcase/buildlogic/KotlinConventionPlugin.kt
+++ buildLogic/src/main/kotlin/com/igorwojda/showcase/buildlogic/KotlinConventionPlugin.kt
@@ -3,7 +3,9 @@ package com.igorwojda.showcase.buildlogic
 import com.igorwojda.showcase.buildlogic.config.JavaBuildConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class KotlinConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,6 +16,19 @@ class KotlinConventionPlugin : Plugin<Project> {
             }
 
             kotlinExtension.jvmToolchain(JavaBuildConfig.JVM_TOOLCHAIN_VERSION)
+
+            tasks.withType<KotlinCompile>().configureEach {
+                compilerOptions {
+                    freeCompilerArgs.addAll(
+                        /*
+                        This ensures annotations on data class constructor parameters are applied to both
+                        the parameter and the backing field, preventing future breaking changes.
+                        See https://youtrack.jetbrains.com/issue/KT-73255: for more details.
+                         */
+                        "-Xannotation-default-target=param-property",
+                    )
+                }
+            }
         }
     }
 }
