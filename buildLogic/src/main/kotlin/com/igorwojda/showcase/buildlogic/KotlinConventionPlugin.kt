b6b9fc930c03064ae62d25646f00682ddb48430c
Igor Wojda <igor.wojda@gmail.com>
2025-09-16 18:53:02 +0200

    Fix build warning

--- buildLogic/src/main/kotlin/com/igorwojda/showcase/buildlogic/KotlinConventionPlugin.kt
+++ buildLogic/src/main/kotlin/com/igorwojda/showcase/buildlogic/KotlinConventionPlugin.kt
@@ -3,7 +3,9 @@ package com.igorwojda.showcase.buildlogic
 import com.igorwojda.showcase.buildlogic.config.JavaBuildConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class KotlinConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,6 +16,15 @@ class KotlinConventionPlugin : Plugin<Project> {
             }
 
             kotlinExtension.jvmToolchain(JavaBuildConfig.JVM_TOOLCHAIN_VERSION)
+
+            tasks.withType<KotlinCompile>().configureEach {
+                compilerOptions {
+                    freeCompilerArgs.addAll(
+                                    // Fix for https://youtrack.jetbrains.com/issue/KT-73255: set default annotation target
+                        "-Xannotation-default-target=param-property"
+                    )
+                }
+            }
         }
     }
 }
