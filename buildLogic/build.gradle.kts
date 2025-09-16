cfd8ef325bcd83467b6791f241501172fe90ec84
Igor Wojda <igor.wojda@gmail.com>
2025-09-16 19:03:09 +0200

    Rename plugins

--- buildLogic/build.gradle.kts
+++ buildLogic/build.gradle.kts
@@ -10,11 +10,10 @@ group = "com.igorwojda.showcase.buildlogic"
 Configure the buildLogic plugins to target JDK from version catalog
 This matches the JDK used to build the project, and is not related to what is running on device.
 */
-val javaVersion =
-    libs
-        .versions
-        .java
-        .get()
+val javaVersion = libs
+    .versions
+    .java
+    .get()
 
 kotlin {
     compilerOptions {
@@ -30,10 +29,10 @@ dependencies {
     implementation(libs.ksp.gradlePlugin)
     implementation(libs.spotless.gradlePlugin)
     implementation(libs.detekt.gradlePlugin)
-    implementation(libs.testLogger.gradlePlugin)
+    implementation(libs.test.logger.gradlePlugin)
     implementation(libs.compose.gradlePlugin)
     implementation(libs.junit5.gradlePlugin)
-    implementation(libs.easyLauncher.gradlePlugin)
+    implementation(libs.easy.launcher.gradlePlugin)
 
     /*
     Expose generated type-safe version catalogs accessors accessible from precompiled script plugins
