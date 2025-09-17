3be39f0533d97f86f868866ece8c11099639e516
Igor Wojda <igor.wojda@gmail.com>
2025-09-17 16:59:36 +0200

    Fix tests

--- feature/settings/src/test/kotlin/com/igorwojda/showcase/feature/settings/presentation/screen/settings/SettingsViewModelTest.kt
+++ feature/settings/src/test/kotlin/com/igorwojda/showcase/feature/settings/presentation/screen/settings/SettingsViewModelTest.kt
@@ -1,3 +1,22 @@
 package com.igorwojda.showcase.feature.settings.presentation.screen.settings
 
-class SettingsViewModelTest
+import com.igorwojda.showcase.library.testutils.CoroutinesTestDispatcherExtension
+import com.igorwojda.showcase.library.testutils.InstantTaskExecutorExtension
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(InstantTaskExecutorExtension::class, CoroutinesTestDispatcherExtension::class)
+class SettingsViewModelTest {
+    private val sut = SettingsViewModel()
+
+    @Test
+    fun `initial state should be Content`() =
+        runTest {
+            // then
+            sut.uiStateFlow.value shouldBeEqualTo SettingsUiState.Content
+        }
+}
