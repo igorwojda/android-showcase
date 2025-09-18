8dd818e29670c6b0fde73cf85bb7c89f2b4d49e3
Igor Wojda <igor.wojda@gmail.com>
2025-09-18 13:39:30 +0200

    Run Konsist Test only

--- konsistTest/build.gradle.kts
+++ konsistTest/build.gradle.kts
@@ -13,3 +13,17 @@ dependencies {
     testImplementation(libs.bundles.test)
     testImplementation(libs.viewmodel.ktx)
 }
+
+// Enable tests only for 'konsistTest:test' tasks but disable for others like 'testDebugUnitTest'
+tasks.withType<Test>().configureEach {
+    val taskPath = path
+
+    enabled = when {
+        // Enable if it's the konsistTest module's test task
+        taskPath.contains("konsistTest:test") -> true
+        // Default behavior for other test tasks
+        // You can set this to false to disable by default, or true to enable by default
+        else -> false
+    }
+}
+
