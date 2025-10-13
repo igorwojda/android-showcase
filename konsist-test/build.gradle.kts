2fc674b1ea771773e3b072cb2c5122d88f01a728
Igor Wojda <igor.wojda@gmail.com>
2025-10-13 12:35:05 +0200

    Update Konsist config

--- konsist-test/build.gradle.kts
+++ konsist-test/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(projects.feature.base)
 
     testImplementation(projects.library.testUtils)
+    testImplementation(libs.bundles.test)
     testImplementation(libs.konsist)
     testImplementation(libs.viewmodel.ktx)
 }
