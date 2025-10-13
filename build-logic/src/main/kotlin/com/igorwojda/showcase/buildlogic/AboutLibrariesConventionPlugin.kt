0b9e09a220e8003f2e2c2db955262074ca2cae7b
Igor Wojda <igor.wojda@gmail.com>
2025-10-13 02:38:53 -0700

    Upgrade about libraries (#737)

--- build-logic/src/main/kotlin/com/igorwojda/showcase/buildlogic/AboutLibrariesConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/igorwojda/showcase/buildlogic/AboutLibrariesConventionPlugin.kt
@@ -1,19 +1,13 @@
 package com.igorwojda.showcase.buildlogic
 
-import com.mikepenz.aboutlibraries.plugin.AboutLibrariesExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 
 class AboutLibrariesConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("com.mikepenz.aboutlibraries.plugin")
-            }
-
-            extensions.configure<AboutLibrariesExtension> {
-                android.registerAndroidTasks.set(true)
+                apply("com.mikepenz.aboutlibraries.plugin.android")
             }
         }
     }
